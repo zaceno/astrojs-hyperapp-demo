d6dc638be930bc42fcb3b86abb70b827a6386ede
Zacharias Enochsson <zacharias.enochsson@gmail.com>
2023-10-23 14:43:07 +0200

    add node adapter so builds work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from "astro/config"
-import hyperapp from "@zxlabs/astrojs-hyperapp"
+import { defineConfig } from "astro/config";
+import hyperapp from "@zxlabs/astrojs-hyperapp";
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [hyperapp()],
   output: "hybrid",
-})
+  adapter: node({
+    mode: "standalone"
+  })
+});
